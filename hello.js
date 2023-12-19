ff42dc17ad1f4fa507ac59e17effe7d55a4b1764
unknown <mmcadoulos@gmail.com>
2023-12-18 20:35:27 -0600

    attempted problems 5-8 on line 8 previously. re-did assignments on line 7 for January class

--- hello.js
+++ hello.js
@@ -4,6 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
+console.log('Hello World!'); console.log("message 2", '"electric '+'boogaloo"')
 console.log('I shall type up the number "8"',"on a separate line by using an equation");console.log(256/32)
 
 /*  
@@ -11,12 +12,19 @@ Next, PLAY!
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
+      Done
    2. Figure out what the parentheses do. Will the code work without them?
+      No
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+      Opening and closing are necessary. No difference between ' and "
    4. Remove the semi-colon, ;.
+      Semi-colon ends line
    5. Print a number. (Bonus: Print two numbers added together).
+      Print numbers/equations by typing inside without quotes
    6. Print multiple messages one after the other.
+      console.log("message 1"+"message 2")
    7. Print two messages on the same line.
+      console.log("message 1", "message 2")
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
    9. Other. You choose!
 */
